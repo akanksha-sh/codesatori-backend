b9a191a7503890fa8804696c5b9f10d1f38c4bdb
Luke  Thorpe <luke.thorpe18@imperial.ac.uk>
2020-05-27 17:11:34 +0100

    Initial commit; setup Maven project with Spring Boot 2

---
+++ src/main/java/uk/co/codesatori/backend/CodeSatoriBackEndApplication.java
@@ -0,0 +1,13 @@
+package uk.co.codesatori.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeSatoriBackEndApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CodeSatoriBackEndApplication.class, args);
+  }
+
+}
---
+++ src/test/java/uk/co/codesatori/backend/CodeSatoriBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.co.codesatori.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeSatoriBackEndApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
